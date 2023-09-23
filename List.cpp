1a7bd80319a85758c94eb7f7d01dec820d3939c5
jakujobi <akujobi365@gmail.com>
2023-09-23 11:47:56 -0500

    Create List.cpp
    This contains the Implementation Files

---
+++ List.cpp
@@ -0,0 +1,29 @@
+//Implementation Files (.cpp) 
+/******************************************************************** 
+*** NAME : JOHN AKUJOBI    *** 
+*** CLASS : CSC 300 - DATA STRUCTURES*** 
+*** ASSIGNMENT :                               *** 
+*** DUE DATE : ####                                       *** 
+*** INSTRUCTOR : GAMRADT *** 
+********************************************************************* 
+*** DESCRIPTION : <detailed english description of the abstract data type> *** 
+*** <including supporting operations> *** 
+********************************************************************/ 
+
+#include “myadt.h” // matching header file 
+// other preprocessor directives 
+/******************************************************************** 
+*** FUNCTION <name of function> *** 
+********************************************************************* 
+*** DESCRIPTION : <detailed english description of the function> *** 
+*** INPUT ARGS : <list of all input argument names> *** 
+*** OUTPUT ARGS : <list of all output argument names> *** 
+*** IN/OUT ARGS : <list of all input/output argument names> *** 
+*** RETURN : <return type and return value name> *** 
+********************************************************************/ 
+ReturnType MyClass::functionName( <argument declarations> ) 
+{ 
+// etc… 
+} 
+//Note: 
+//• a separate function description block is required for each function definition.
