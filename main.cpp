4aca8a556249a64734cdb9dd9a4c0571632d2d0b
jakujobi <akujobi365@gmail.com>
2023-09-23 12:11:31 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+// Driver/main Files (.cpp) 
+/******************************************************************** 
+*** NAME : JOHN AKUJOBI    *** 
+*** CLASS : CSC 300 - DATA STRUCTURES*** 
+*** ASSIGNMENT : ######## *** 
+*** DUE DATE : ####                                       *** 
+*** INSTRUCTOR : GAMRADT *** 
+*********************************************************************
+*** DESCRIPTION : <detailed english description of the current assignment> *** 
+********************************************************************/ 
+
+// preprocessor directives 
+// global declarations and definitions – constants, data types, functions 
+// main function definition 
+
+/******************************************************************** 
+*** FUNCTION <name of function> *** 
+********************************************************************* 
+*** DESCRIPTION : <detailed english description of the function> *** 
+*** INPUT ARGS : <list of all input argument names> *** 
+*** OUTPUT ARGS : <list of all output argument names> *** 
+*** IN/OUT ARGS : <list of all input/output argument names> *** 
+*** RETURN : <return type and return value name> *** 
+********************************************************************/ 
+
+ReturnType functionName( <argument declarations> ) 
+{ 
+// etc… 
+} 
+
+//Notes: 
+//• readable, self-describing identifier names are required. 
+//• a separate function description block is required for each function definition other than main. 
