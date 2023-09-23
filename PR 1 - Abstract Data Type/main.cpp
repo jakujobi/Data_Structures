f9282fc8c5660c9cbcf77fe5dcae2321a479aa0a
jakujobi <akujobi365@gmail.com>
2023-09-23 16:04:16 -0500

    Changed the top block of comments
    Included the Assignment number and due date

--- PR 1 - Abstract Data Type/main.cpp
+++ PR 1 - Abstract Data Type/main.cpp
@@ -1,10 +1,10 @@
 // Driver/main Files (.cpp) 
 /******************************************************************** 
-*** NAME : JOHN AKUJOBI    *** 
-*** CLASS : CSC 300 - DATA STRUCTURES*** 
-*** ASSIGNMENT : ######## *** 
-*** DUE DATE : ####                                       *** 
-*** INSTRUCTOR : GAMRADT *** 
+*** NAME : JOHN AKUJOBI                                           *** 
+*** CLASS : CSC 300 - DATA STRUCTURES                             *** 
+*** ASSIGNMENT : 1                                                *** 
+*** DUE DATE : 09-25-23                                           *** 
+*** INSTRUCTOR : GAMRADT                                          *** 
 *********************************************************************
 *** DESCRIPTION : <detailed english description of the current assignment> *** 
 ********************************************************************/ 
@@ -12,21 +12,6 @@
 // preprocessor directives 
 // global declarations and definitions – constants, data types, functions 
 // main function definition 
-
-/******************************************************************** 
-*** FUNCTION <name of function> *** 
-********************************************************************* 
-*** DESCRIPTION : <detailed english description of the function> *** 
-*** INPUT ARGS : <list of all input argument names> *** 
-*** OUTPUT ARGS : <list of all output argument names> *** 
-*** IN/OUT ARGS : <list of all input/output argument names> *** 
-*** RETURN : <return type and return value name> *** 
-********************************************************************/ 
-
-// ReturnType functionName( <argument declarations> ) 
-// { 
-// // etc… 
-// } 
 
 //Notes: 
 //• readable, self-describing identifier names are required. 
