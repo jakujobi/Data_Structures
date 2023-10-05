d2c68e6a8d708719a1d8f023db69386d6c90507a
jakujobi <akujobi365@gmail.com>
2023-10-04 20:00:34 -0500

    Update stack.cpp

--- PR2-Stack/stack.cpp
+++ PR2-Stack/stack.cpp
@@ -9,10 +9,29 @@ class Stack {
 public:
     Stack();    // default constructor
     
-    Stack(int capacity);   // constructor with initial capacity
+    Stack(int capacity);   // constructor with initial capacity as parameter
 
     Stack(const Stack& other);    // copy constructor
 
     ~Stack();   // destructor
     
     void push(const string& element);   // add an element as a string to the stack
+
+    void pop(); // remove the top element from the stack
+
+    string peek() const; // return the top element from the stack
+
+    bool empty() const; // return true if the stack is empty
+
+    bool full() const; // return true if the stack is full
+
+    int size() const; // return the number of elements in the stack
+
+    void view() const; // print the stack
+
+    Stack copy() const; // return a copy of the stack
+
+    void destroy(); // deallocate the stack and removes all elements
+
+private:
+    static const int STACK_SIZE = 2;
