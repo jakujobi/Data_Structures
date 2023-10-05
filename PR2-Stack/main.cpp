0a8d13d9ffa9f01cf69283bf588ee52bf41840f6
jakujobi <akujobi365@gmail.com>
2023-10-04 20:18:49 -0500

    New

---
+++ PR2-Stack/main.cpp
@@ -0,0 +1,11 @@
+//print hello world
+
+#include <iostream>
+#include "stack.h"
+#include <string>
+using namespace std;
+
+int main(){
+    cout << "hello world";
+    return 0;
+}
--- PR2-Stack/stack.cpp
+++ PR2-Stack/stack.cpp
@@ -1,37 +1,48 @@
-#ifndef __STACK_H__
-#define __STACK_H__
+// #ifndef __STACK_H__
+// #define __STACK_H__
 
-#include <iostream>
-#include <string>
-using namespace std;
+// #include <iostream>
+// #include <string>
+// using namespace std;
 
-class Stack {
-public:
-    Stack();    // default constructor
+// class Stack {
+// public:
+//     Stack();    // default constructor
     
-    Stack(int capacity);   // constructor with initial capacity as parameter
+//     Stack(int capacity);   // constructor with initial capacity as parameter
 
-    Stack(const Stack& other);    // copy constructor
+//     Stack(const Stack& other);    // copy constructor
 
-    ~Stack();   // destructor
+//     ~Stack();   // destructor
     
-    void push(const string& element);   // add an element as a string to the stack
+//     void push(const string& element);   // add an element as a string to the stack
 
-    void pop(); // remove the top element from the stack
+//     void pop(); // remove the top element from the stack
 
-    string peek() const; // return the top element from the stack
+//     string peek() const; // return the top element from the stack
 
-    bool empty() const; // return true if the stack is empty
+//     bool empty() const; // return true if the stack is empty
 
-    bool full() const; // return true if the stack is full
+//     bool full() const; // return true if the stack is full
 
-    int size() const; // return the number of elements in the stack
+//     int size() const; // return the number of elements in the stack
 
-    void view() const; // print the stack
+//     void view() const; // print the stack
 
-    Stack copy() const; // return a copy of the stack
+//     Stack copy() const; // return a copy of the stack
 
-    void destroy(); // deallocate the stack and removes all elements
+//     void destroy(); // deallocate the stack and removes all elements
 
-private:
-    static const int STACK_SIZE = 2;
+// private:
+//     static const int STACK_SIZE = 2;
+    
+//     string* stackArray;         // Pointer to dynamic array
+
+//     int top;                         // Index of top element
+
+//     void initialize();  // initialize the stack to empty
+
+//     void resize();  // Resize the stack array when it's full
+// };
+
+// #endif // STACK_H 
