89e4d70886ebf5b6964c33306bfa86c5a3a79b8e
jakujobi <akujobi365@gmail.com>
2023-10-04 21:49:09 -0500

    Added new functions (resize)
    I reviewed the assignment requirements and realized that i needed a resize function.
    I created it.
    
    I debugged by code using an offline LLM which suggested better ways of rewriting some of the functions there
    
    I also used Claude ai to better understand the requirements set down by the professor. (The initial one from the class was a bit hard to breakdown)
    
    I also added the throw, and made the array double in size every time it reaches its size limit

--- PR2-Stack/PR2_StackVS/Stack.cpp
+++ PR2-Stack/PR2_StackVS/Stack.cpp
@@ -1,19 +1,116 @@
+// #include "Stack.h"
+// #include <iostream>
+// using namespace std;
+
+// // Constructor (default size is 2)
+// Stack::Stack(int capacity) {
+//     initialize();
+//     if (capacity > 0) {
+//         stackArray = new std::string[capacity];
+//     }
+// }
+
+// // Copy constructor
+// Stack::Stack(const Stack& other) {
+//     initialize();
+//     stackArray = new std::string[other.STACK_SIZE];
+//     top = other.top;
+//     for (int i = 0; i <= top; ++i) {
+//         stackArray[i] = other.stackArray[i];
+//     }
+// }
+
+// // Destructor
+// Stack::~Stack() {
+//     delete[] stackArray;
+// }
+
+// // Initialize stack to empty
+// void Stack::initialize() {
+//     stackArray = nullptr;
+//     top = -1;
+// }
+
+// // Add element to top
+// void Stack::push(const std::string& element) {
+//     if (full()) {
+//         std::cout << "Warning: Stack is full. Cannot push element." << std::endl;
+//         return;
+//     }
+//     stackArray[++top] = element;
+// }
+
+// // Remove element from top
+// void Stack::pop() {
+//     if (empty()) {
+//         std::cout << "Warning: Stack is empty. Cannot pop element." << std::endl;
+//         return;
+//     }
+//     --top;
+// }
+
+// // Access top element
+// std::string Stack::peek() const {
+//     if (empty()) {
+//         std::cout << "Warning: Stack is empty. Cannot peek." << std::endl;
+//         return "";
+//     }
+//     return stackArray[top];
+// }
+
+// // Display stack contents
+// void Stack::view() const {
+//     for (int i = top; i >= 0; --i) {
+//         std::cout << stackArray[i] << std::endl;
+//     }
+// }
+
+// // Build a separate duplicate copy of the existing stack
+// Stack Stack::copy() const {
+//     Stack copiedStack(STACK_SIZE);
+//     for (int i = top; i >= 0; --i) {
+//         copiedStack.push(stackArray[i]);
+//     }
+//     return copiedStack;
+// }
+
+// // Remove all elements from the stack
+// void Stack::destroy() {
+//     while (!empty()) {
+//         pop();
+//     }
+// }
+
+// // Checks if the stack is empty
+// bool Stack::empty() const {
+//     return top == -1;
+// }
+
+// // Checks if the stack is full
+// bool Stack::full() const {
+//     return top == STACK_SIZE - 1;
+// }
+
+
+
+//!________________________________________________________________________
 #include "Stack.h"
 #include <iostream>
+#include <string>
 using namespace std;
 
 // Constructor (default size is 2)
-Stack::Stack(int capacity) {
+Stack::Stack(int capacity) : STACK_SIZE(capacity) {
     initialize();
     if (capacity > 0) {
-        stackArray = new std::string[capacity];
+        stackArray = new Element[STACK_SIZE];
     }
 }
 
 // Copy constructor
-Stack::Stack(const Stack& other) {
+Stack::Stack(const Stack& other) : STACK_SIZE(other.STACK_SIZE) {
     initialize();
-    stackArray = new std::string[other.STACK_SIZE];
+    stackArray = new Element[other.STACK_SIZE];
     top = other.top;
     for (int i = 0; i <= top; ++i) {
         stackArray[i] = other.stackArray[i];
@@ -32,37 +129,74 @@ void Stack::initialize() {
 }
 
 // Add element to top
-void Stack::push(const std::string& element) {
-    if (full()) {
-        std::cout << "Warning: Stack is full. Cannot push element." << std::endl;
-        return;
+// Push element onto the stack
+void Stack::push(const Element& element) {
+    if (top == STACK_SIZE - 1) {
+        resize();
     }
     stackArray[++top] = element;
+    // if (full()) {
+    //     std::cout << "Warning: Stack is full. Cannot push element." << std::endl;
+    //     return;
 }
 
 // Remove element from top
-void Stack::pop() {
+Element Stack::pop() {
+    // if (empty()) {
+    //     std::cout << "Warning: Stack is empty. Cannot pop element." << std::endl;
+    //     return;
+    // }
+    // --top;
     if (empty()) {
-        std::cout << "Warning: Stack is empty. Cannot pop element." << std::endl;
-        return;
+        throw std::runtime_error("Stack is empty. Cannot pop.");
     }
-    --top;
+    return stackArray[top--];
 }
 
 // Access top element
-std::string Stack::peek() const {
-    if (empty()) {
-        std::cout << "Warning: Stack is empty. Cannot peek." << std::endl;
-        return "";
+Element Stack::peek() const {
+    // if (empty()) {
+    //     std::cout << "Warning: Stack is empty. Cannot peek." << std::endl;
+    //     return "";
+    // }
+        if (empty()) {
+        throw std::runtime_error("Stack is empty. Cannot peek.");
     }
     return stackArray[top];
 }
 
 // Display stack contents
 void Stack::view() const {
-    for (int i = top; i >= 0; --i) {
-        std::cout << stackArray[i] << std::endl;
+    // for (int i = top; i >= 0; --i) {
+    //     std::cout << stackArray[i] << std::endl;
+    // }
+    if (empty()) {
+        cout << "TOP -> BOTTOM // Output for an empty Stack instance" << endl;
+    } else {
+        cout << "TOP -> ";
+        for (int i = top; i >= 0; --i) {
+            cout << stackArray[i];
+            if (i > 0) {
+                cout << " -> ";
+            } else {
+                cout << " -> BOTTOM // Output for a populated Stack instance" << endl;
+            }
+        }
     }
+}
+
+// Resize the stack array when it's full
+void Stack::resize() {
+    int newCapacity = STACK_SIZE * 2; // Double the capacity
+    Element* newStack = new Element[newCapacity];
+    
+    for (int i = 0; i <= top; ++i) {
+        newStack[i] = stackArray[i];
+    }
+
+    delete[] stackArray; // Deallocate the old array
+    stackArray = newStack;
+    STACK_SIZE = newCapacity;
 }
 
 // Build a separate duplicate copy of the existing stack
--- PR2-Stack/PR2_StackVS/Stack.h
+++ PR2-Stack/PR2_StackVS/Stack.h
@@ -1,8 +1,60 @@
+// #ifndef _STACK_H
+// #define _STACK_H
+// #include <string>
+// #include <iostream>
+// using namespace std;
+
+// class Stack {
+// public:
+//     Stack(int capacity = 2);   // default constructor with initial capacity as parameter
+
+//     Stack(const Stack& other);    // copy constructor
+
+//     ~Stack();   // destructor
+    
+//     void push(const string& element);   // add an element as a string to the stack
+
+//     void pop(); // remove the top element from the stack
+
+//     string peek() const; // return the top element from the stack
+
+//     bool empty() const; // return true if the stack is empty
+
+//     bool full() const; // return true if the stack is full
+
+//     int size() const; // return the number of elements in the stack
+
+//     void view() const; // print the stack
+
+//     Stack copy() const; // return a copy of the stack
+
+//     void destroy(); // deallocate the stack and removes all elements
+
+// private:
+//     static const int STACK_SIZE = 2;
+    
+//     string* stackArray;         // Pointer to dynamic array
+
+//     int top;  // Index of top element
+
+//     void initialize();  // initialize the stack to empty
+
+//     void resize();  // Resize the stack array when it's full
+
+//     int stackSize; // keep track of the number of elements in the stack
+// };
+
+// #endif // STACK_H 
+
+
+//!------------------------------------------
 #ifndef _STACK_H
 #define _STACK_H
 #include <string>
 #include <iostream>
 using namespace std;
+
+typedef string Element;
 
 class Stack {
 public:
@@ -12,11 +64,11 @@ public:
 
     ~Stack();   // destructor
     
-    void push(const string& element);   // add an element as a string to the stack
+    void push(const Element& element);   // add an element as a string to the stack
 
-    void pop(); // remove the top element from the stack
+    Element pop(); // remove the top element from the stack
 
-    string peek() const; // return the top element from the stack
+    Element peek() const; // return the top element from the stack
 
     bool empty() const; // return true if the stack is empty
 
@@ -31,9 +83,9 @@ public:
     void destroy(); // deallocate the stack and removes all elements
 
 private:
-    static const int STACK_SIZE = 2;
+    int STACK_SIZE;
     
-    string* stackArray;         // Pointer to dynamic array
+    Element* stackArray;         // Pointer to dynamic array
 
     int top;  // Index of top element
 
