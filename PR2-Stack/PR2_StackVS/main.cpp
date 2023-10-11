5f59448522f327e519f41652a0524e752eec2e87
jakujobi <akujobi365@gmail.com>
2023-10-11 11:44:54 -0500

    Added new main program with 6 test cases

--- PR2-Stack/PR2_StackVS/main.cpp
+++ PR2-Stack/PR2_StackVS/main.cpp
@@ -1,7 +1,88 @@
 //main.cpp
 
+//!________________________________________________
+// Driver/main Files (.cpp) 
+/******************************************************************** 
+*** NAME : JOHN AKUJOBI                                         *** 
+*** CLASS : CSC 300 - DATA STRUCTURES                           *** 
+*** ASSIGNMENT : Assignment #2 - Stack Testing                  *** 
+*** DUE DATE : 10-06-23                                         *** 
+*** INSTRUCTOR : GAMRADT                                        *** 
+*********************************************************************/
+/********************************************************************* 
+*** DESCRIPTION : This program tests the Stack class to ensure that ***
+                 every operation works as expected.                 ***
+*********************************************************************/
+#include "Stack.h"
+#include <iostream>
+using namespace std;
 
+int main() {
+    /********************************************************************
+    *** Test Case 1: Testing Default Constructor and Push            ***
+    *********************************************************************/
+    cout << "Test Case 1: Default Constructor and Push\n";
+    Stack defaultStack; // Create a stack using default constructor and default capacity
+    defaultStack.push("CSc"); // Push some elements onto the stack
+    defaultStack.push("300");
+    defaultStack.push("Data");
+    defaultStack.view(); // Expected: TOP -> Data -> 300 -> CSc -> BOTTOM
+    cout << "------------------------------------\n";
 
+    /********************************************************************
+    *** Test Case 2: Testing Parameterized Constructor and Resize     ***
+    *********************************************************************/
+    cout << "Test Case 2: Parameterized Constructor and Resize\n";
+    Stack paramStack(1);    // Create a stack using parameterized constructor and given capacity of just 1
+    paramStack.push("Structures");
+    paramStack.push("ADT"); // Here it might resize if implemented correctly
+    paramStack.view(); // Expected: TOP -> ADT -> Structures -> BOTTOM
+    cout << "------------------------------------\n";
+
+    /********************************************************************
+    *** Test Case 3: Testing Copy Constructor                         ***
+    *********************************************************************/
+    cout << "Test Case 3: Copy Constructor\n";
+    Stack copyOfDefaultStack = defaultStack; // Create a copy of the default stack
+    copyOfDefaultStack.view(); // Expected: TOP -> Data -> 300 -> CSc -> BOTTOM
+    cout << "------------------------------------\n";
+
+    /********************************************************************
+    *** Test Case 4: Testing Pop and Peek                             ***
+    *********************************************************************/
+    cout << "Test Case 4: Pop and Peek\n";
+    Element topElement = defaultStack.pop();    // Pop the top element
+    cout << "Popped Element: " << topElement << endl; // Expected: Data
+    cout << "Peek after pop: " << defaultStack.peek() << endl; // Expected: 300
+    defaultStack.view(); // Expected: TOP -> 300 -> CSc -> BOTTOM
+    cout << "------------------------------------\n";
+
+    /********************************************************************
+    *** Test Case 5: Test Edge Case (Push on Full Stack)               ***
+    *********************************************************************/
+    cout << "Test Case 5: Push on Full Stack\n";
+    // Depending on your implementation, this might show resize option
+    paramStack.push("Stack");   // Push on full stack
+    paramStack.push("Overflow");
+    paramStack.view(); 
+    cout << "------------------------------------\n";
+
+    /********************************************************************
+    *** Test Case 6: Test Edge Case (Pop from Empty Stack)             ***
+    *********************************************************************/
+    cout << "Test Case 6: Pop from Empty Stack\n";
+    Stack emptyStack;
+    try {
+        emptyStack.pop();
+    } catch (const runtime_error& e) {
+        cout << e.what() << endl; // Expected: Stack is empty. Cannot pop.
+    }
+    cout << "------------------------------------\n";
+
+    //********************************************************************
+
+    return 0;
+}
 
 
 //! The old main function
