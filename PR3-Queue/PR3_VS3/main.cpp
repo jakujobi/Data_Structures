3bae74666a3c692eec44c7794c2d8dc6ec528568
jakujobi <akujobi365@gmail.com>
2023-10-16 23:44:44 -0500

    Created 5 test cases

--- PR3-Queue/PR3_VS3/main.cpp
+++ PR3-Queue/PR3_VS3/main.cpp
@@ -1,9 +1,57 @@
-#include <iostream>
+//Description: This program tests the Queue class.
+/*
+Test Cases Included:
+Default Constructor and Enqueue:
+	Creates a queue using the default constructor and enqueues some elements.
+Parameterized Constructor:
+	Creates a queue using a parameterized constructor with a user-specified size.
+Copy Constructor:
+	Creates a copy of an existing queue.
+Dequeue:
+	Dequeues an element and shows the dequeued element.
+Destructor:
+	Tests the destructor by creating a queue in a local scope
+*/
 
+#include "Queue.h"
+#include <iostream>
 using namespace std;
 
 int main() {
-	cout << "Hello World!" << endl;
+    cout << "Test Case 1: Default Constructor and Enqueue\n";
+    Queue defaultQueue;
+    defaultQueue.enqueue(5);
+    defaultQueue.enqueue(-3);
+    defaultQueue.view();
+    cout << "------------------------------------\n";
 
-	return 0;
+    cout << "Test Case 2: Parameterized Constructor\n";
+    Queue paramQueue(5);
+    paramQueue.enqueue(10);
+    paramQueue.enqueue(20);
+    paramQueue.view();
+    cout << "------------------------------------\n";
+
+    cout << "Test Case 3: Copy Constructor\n";
+    Queue copyQueue = defaultQueue;
+    copyQueue.view();
+    cout << "------------------------------------\n";
+
+    cout << "Test Case 4: Dequeue\n";
+    Element dequeuedElem;
+    defaultQueue.dequeue(dequeuedElem);
+    cout << "Dequeued Element: " << dequeuedElem << endl;
+    defaultQueue.view();
+    cout << "------------------------------------\n";
+
+    cout << "Test Case 5: Destructor\n";
+    {
+        Queue destructorQueue;
+        destructorQueue.enqueue(100);
+        destructorQueue.enqueue(200);
+    }
+    cout << "Destructor ran successfully.\n";
+    cout << "------------------------------------\n";
+
+    return 0;
 }
