954a57f59e73aeb97be01fff386fb030c7a92df1
jakujobi <akujobi365@gmail.com>
2023-10-17 09:18:41 -0500

    Modified the view, and constructor functions

--- PR3-Queue/PR3_VS3/Queue.cpp
+++ PR3-Queue/PR3_VS3/Queue.cpp
@@ -8,18 +8,31 @@ Queue::Queue(int size) : QUEUE_SIZE(size), head(0), tail(0) {
     queueArray = new Element[QUEUE_SIZE]; // Allocate dynamic array
 }
 
-Queue::Queue( Queue & other) : QUEUE_SIZE (other.QUEUE_SIZE) { //gotten from last assignment
-    queueArray = new Element[QUEUE_SIZE]; // Allocate dynamic array
-    
-    // Copy elements from the other queue
-    for (int i = 0; i < QUEUE_SIZE; ++i) {
-        queueArray[i] = other.queueArray[i];
+// Queue::Queue( Queue & other) : QUEUE_SIZE (other.QUEUE_SIZE) { //gotten from last assignment
+//     queueArray = new Element[QUEUE_SIZE]; // Allocate dynamic array
+//     // Copy elements from the other queue
+//     for (int i = 0; i < QUEUE_SIZE; ++i) {
+//         queueArray[i] = other.queueArray[i];
+//     }
+// }
+
+Queue::Queue(Queue &other) : QUEUE_SIZE(other.QUEUE_SIZE), head(other.head), tail(other.tail) {
+    queueArray = new Element[QUEUE_SIZE];
+    // Copy elements
+    for (short i = head; i != tail; i = (i + 1) % QUEUE_SIZE) {
+        enqueue(other.queueArray[i]);
     }
 }
 
+
 //!Destructor
 Queue::~Queue() {
-    delete[] queueArray;    // Deallocate the dynamic array
+    //pop off all the items in the queue
+    while (!isEmpty()) {
+        Element temp;
+        dequeue(temp);
+    }
+    delete[] queueArray;   // Deallocate the dynamic array
     queueArray = nullptr; // Avoid the dangling pointer
 }
 
@@ -42,13 +55,29 @@ void Queue::dequeue(Element &stuff) {
     }
 }
 
+// void Queue::view() {
+//     cout << "HEAD -> "; //print HEAD in front like the assignment project shows
+//     while (head != tail) {
+//         cout << queueArray[head] << " -> ";
+//         head = (head + 1) % QUEUE_SIZE;
+//     }
+//     for (int i = head; i != tail; i = (i + 1) % QUEUE_SIZE) {
+//         cout << queueArray[i] << " -> "; 
+//     }
+//     cout << "TAIL\n"; //print TAIL behind also like the assignment project shows
+// }
+
 void Queue::view() {
-    cout << "HEAD -> "; //print HEAD in front like the assignment project shows
-    for (int i = head; i != tail; i = (i + 1) % QUEUE_SIZE) {
-        cout << queueArray[i] << " -> "; 
+    cout << "HEAD -> ";
+    for (short i = head; i != tail; i = (i + 1) % QUEUE_SIZE) {
+        Element temp;
+        dequeue(temp);
+        cout << temp << " -> ";
+        enqueue(temp);
     }
-    cout << "TAIL\n"; //print TAIL behind also like the assignment project shows
+    cout << "TAIL" << endl;
 }
+
 
 bool Queue::isEmpty() const {
     return head == tail;    // If head and tail are equal, the queue is empty
