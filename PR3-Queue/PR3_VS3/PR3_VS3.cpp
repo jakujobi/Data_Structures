8a88e520d442b1680d1ec5dc26b9a98eeb39b50a
jakujobi <akujobi365@gmail.com>
2023-10-16 22:37:17 -0500

    Created a Visual Studio project for Project 3

---
+++ PR3-Queue/PR3_VS3/PR3_VS3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "main.cpp" // Include main.cpp (assuming main.cpp contains your functions)
+using namespace std;
+
+int PR3_VS3() { //! Change to the name of the project file
+
+    main(); // Call main() from main.cpp
+
+    // You can call any functions or perform any actions you need here
+    cout << "Top-level program running." << endl;
+
+    return 0;
+}
---
+++ PR3-Queue/PR3_VS3/Queue.h
@@ -0,0 +1,24 @@
+#ifndef _QUEUE_H // Guard
+#define _QUEUE_H
+
+typedef int Element;
+typedef Element * ElementPtr;
+
+class Queue {
+    public:     // exportable
+        // General description of each of the ADT operations/functions – exportable operations only
+        explicit Queue( … );        // replace … with required arguments
+        Queue( Queue & );       // reuse enqueue & dequeue
+        ~Queue();      // reuse dequeue
+        void enqueue( const Element );
+        void dequeue( Element & );
+        void view(); // reuse enqueue & dequeue
+    private: // non-exportable
+        // No private member documentation – implementation details are hidden/abstracted away
+        const short QUEUE_SIZE; // requires initialization
+        ElementPtr queueArray;
+        short head, tail;
+        bool isEmpty() const;
+        bool isFull() const;
+};
+#endif // Guard
