534f6bcf1a3f1b3fc07afc058ca9c9f361a603da
jakujobi <akujobi365@gmail.com>
2023-10-16 23:18:29 -0500

    Added the enqueue funuction

--- PR3-Queue/PR3_VS3/Queue.cpp
+++ PR3-Queue/PR3_VS3/Queue.cpp
@@ -16,3 +16,19 @@ Queue::Queue( Queue & other) : QUEUE_SIZE (other.QUEUE_SIZE) { //gotten from las
         queueArray[i] = other.queueArray[i];
     }
 }
+
+//!Destructor
+Queue::~Queue() {
+    delete[] queueArray;    // Deallocate the dynamic array
+    queueArray = nullptr; // Avoid the dangling pointer
+}
+
+Queue::enqueue (const Element item){
+    if (!isFull()){ //cheching if the queue is full, will go ahead if not full
+        queueArray[tail] = item;
+        tail = (tail + 1) % QUEUE_SIZE; // Update tail index
+    }
+    else{
+        cout << "Umm...the queue is full!/nThere is no more room for " << item << endl;
+    }
+}
