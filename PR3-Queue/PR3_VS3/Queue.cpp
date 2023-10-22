47cf751a649e9cbc9fcc7059109d77c2f97a9e47
jakujobi <akujobi365@gmail.com>
2023-10-21 23:28:30 -0500

    Tested program trice
    In the first test, there was an infinite loop from the view function.
    After making changes to it, i ran the second test.
    
    The second test had an infinite loop from the dequeue function
    
    I reversed many of the changes i recently added to the program and executed the 3rd (+ a few more) tests.
    
    They ran successfully

--- PR3-Queue/PR3_VS3/Queue.cpp
+++ PR3-Queue/PR3_VS3/Queue.cpp
@@ -31,13 +31,22 @@ using namespace std;
 // Queue::Queue(int size) : QUEUE_SIZE(size), head(0), tail(0) {
 //     queueArray = new Element[QUEUE_SIZE]; // Allocate dynamic array
 // }
-Queue::Queue(int size) : QUEUE_SIZE(size), head(-1), tail(-1) {
+// Queue::Queue(int size) : QUEUE_SIZE(size), head(-1), tail(-1) {
+//     queueArray = new (nothrow) Element[QUEUE_SIZE];  // Dynamic memory allocation with new (nothrow)
+//     if (!queueArray) {
+//         cerr << "Memory allocation failed." << endl;
+//         exit(1);
+//     }
+// }
+Queue::Queue(int size) : QUEUE_SIZE(size), head(0), tail(0) {
     queueArray = new (nothrow) Element[QUEUE_SIZE];  // Dynamic memory allocation with new (nothrow)
     if (!queueArray) {
         cerr << "Memory allocation failed." << endl;
         exit(1);
     }
 }
+
+
 
 //!Copy Constructor
 /*********************************************************************
@@ -68,23 +77,7 @@ Queue::Queue(int size) : QUEUE_SIZE(size), head(-1), tail(-1) {
 ////Version 4 - Working now
 //Copy constructor - Made sure this followed the instructors pseudocode in the Queue lecture slides
 // Copy constructor
-// Queue::Queue(Queue& other) : QUEUE_SIZE(other.QUEUE_SIZE), head(0), tail(0) {
-//     queueArray = new Element[QUEUE_SIZE];
-//     Queue tempQueue(QUEUE_SIZE); // Temporary Queue
-
-//     // Copy elements from the original queue to the temporary queue
-//     for (short i = other.head; i != other.tail; i = (i + 1) % other.QUEUE_SIZE) {
-//         Element QueueElement = other.queueArray[i];
-//         tempQueue.enqueue(QueueElement); // Temporary Queue
-//     }
-
-//     // Copy elements back to the new queue
-//     for (short i = tempQueue.head; i != tempQueue.tail; i = (i + 1) % tempQueue.QUEUE_SIZE) {
-//         Element QueueElement = tempQueue.queueArray[i];
-//         enqueue(QueueElement); // New Queue
-//     }
-// }
-Queue::Queue(Queue& other) : QUEUE_SIZE(other.QUEUE_SIZE), head(-1), tail(-1) {
+Queue::Queue(Queue& other) : QUEUE_SIZE(other.QUEUE_SIZE), head(0), tail(0) {
     queueArray = new Element[QUEUE_SIZE];
     Queue tempQueue(QUEUE_SIZE); // Temporary Queue
 
@@ -100,6 +93,22 @@ Queue::Queue(Queue& other) : QUEUE_SIZE(other.QUEUE_SIZE), head(-1), tail(-1) {
         enqueue(QueueElement); // New Queue
     }
 }
+// Queue::Queue(Queue& other) : QUEUE_SIZE(other.QUEUE_SIZE), head(-1), tail(-1) {
+//     queueArray = new Element[QUEUE_SIZE];
+//     Queue tempQueue(QUEUE_SIZE); // Temporary Queue
+
+//     // Copy elements from the original queue to the temporary queue
+//     for (short i = other.head; i != other.tail; i = (i + 1) % other.QUEUE_SIZE) {
+//         Element QueueElement = other.queueArray[i];
+//         tempQueue.enqueue(QueueElement); // Temporary Queue
+//     }
+
+//     // Copy elements back to the new queue
+//     for (short i = tempQueue.head; i != tempQueue.tail; i = (i + 1) % tempQueue.QUEUE_SIZE) {
+//         Element QueueElement = tempQueue.queueArray[i];
+//         enqueue(QueueElement); // New Queue
+//     }
+// }
 
 
 //!Destructor
@@ -203,8 +212,8 @@ void Queue::view() {
 *** RETURN : bool - True if empty, false otherwise ***
 *********************************************************************/
 bool Queue::isEmpty() const {
-    //return head == tail;    // If head and tail are equal, the queue is empty
-    return head == -1 && tail == -1; // If head and tail are -1, the queue is empty
+    return head == tail;    // If head and tail are equal, the queue is empty
+    //return head == -1 && tail == -1; // If head and tail are -1, the queue is empty
 }
 
 //! isFull
