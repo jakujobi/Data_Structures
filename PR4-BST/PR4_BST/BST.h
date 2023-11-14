fd0740b57516f62e4492956a3e9b85c309d452b2
jakujobi <akujobi365@gmail.com>
2023-11-13 19:39:46 -0600

    Added comment and documentation

--- PR4-BST/PR4_BST/BST.h
+++ PR4-BST/PR4_BST/BST.h
@@ -30,17 +30,112 @@ struct Node {
 
 class BST {
 public:
-    // Constructor/destructor
+    /// Default constructor
+    /********************************************************************
+    *** FUNCTION BST ***
+    ***********************************************************************
+    *** DESCRIPTION : Constructs an empty Binary Search Tree.           ***
+    *** INPUT ARGS : None                                               ***
+    *** OUTPUT ARGS : None                                              ***
+    *** IN/OUT ARGS : None                                              ***
+    *** RETURN : None                                                   ***
+    ********************************************************************/
     BST();
+
+    // Copy constructor
+    /********************************************************************
+    *** FUNCTION BST (copy constructor) ***
+    *********************************************************************
+    *** DESCRIPTION : Constructs a Binary Search Tree by copying an existing BST. ***
+    *** INPUT ARGS : const BST &source ***
+    *** OUTPUT ARGS : None ***
+    *** IN/OUT ARGS : None ***
+    *** RETURN : None ***
+    ********************************************************************/
     BST(const BST&);
+
+    // Destructor
+    /********************************************************************
+    *** FUNCTION ~BST ***
+    *********************************************************************
+    *** DESCRIPTION : Destroys the Binary Search Tree and deallocates memory. ***
+    *** INPUT ARGS : None ***
+    *** OUTPUT ARGS : None ***
+    *** IN/OUT ARGS : None ***
+    *** RETURN : None ***
+    ********************************************************************/
     ~BST();
-    
-    // ADT operations
+
+    // Insert a new key node to the BST
+    /********************************************************************
+    *** FUNCTION insert ***
+    *********************************************************************
+    *** DESCRIPTION : Inserts a new key node to the BST. ***
+    *** INPUT ARGS : const Element element ***
+    *** OUTPUT ARGS : None ***
+    *** IN/OUT ARGS : None ***
+    *** RETURN : None ***
+    ********************************************************************/
     void insert(const Element);
+
+    // Locate and remove an existing key node from the BST
+    /********************************************************************
+    *** FUNCTION remove ***
+    *********************************************************************
+    *** DESCRIPTION : Locates and removes an existing key node from the BST. ***
+    *** INPUT ARGS : const Element element ***
+    *** OUTPUT ARGS : None ***
+    *** IN/OUT ARGS : None ***
+    *** RETURN : None ***
+    ********************************************************************/
     void remove(const Element);
+
+    // Return a pointer to an existing key node in the BST, otherwise NULL
+    /********************************************************************
+    *** FUNCTION search ***
+    *********************************************************************
+    *** DESCRIPTION : Returns a pointer to an existing key node in the BST, otherwise NULL. ***
+    *** INPUT ARGS : const Element element ***
+    *** OUTPUT ARGS : None ***
+    *** IN/OUT ARGS : None ***
+    *** RETURN : NodePtr ***
+    ********************************************************************/
     NodePtr search(const Element) const;
+
+    // Display the keys in the BST from top to bottom (left to right)
+    //********************************************************************
+    /* FUNCTION preorderView ***
+    *********************************************************************
+    *** DESCRIPTION : Displays the keys in the BST from top to bottom (left to right). ***
+    *** INPUT ARGS : None   ***
+    *** OUTPUT ARGS : None  ***
+    *** IN/OUT ARGS : None  ***
+    *** RETURN : None       ***
+    ********************************************************************/
     void preorderView() const;
+
+    // Display the keys in the BST in ascending order
+    /********************************************************************
+    *** FUNCTION inorderView ***
+    *********************************************************************
+    *** DESCRIPTION : Displays the keys in the BST in ascending order. ***
+    *** INPUT ARGS : None ***
+    *** OUTPUT ARGS : None ***
+    *** IN/OUT ARGS : None ***
+    *** RETURN : None ***
+    ********************************************************************/
     void inorderView() const; 
+
+    // Display the keys in the BST from bottom to top (left to right)
+    /********************************************************************
+    *** FUNCTION postorderView ***
+    *********************************************************************
+    *** DESCRIPTION : Displays the keys in the BST from bottom to top (left to right). ***
+    *** INPUT ARGS : None ***
+    *** OUTPUT ARGS : None ***
+    *** IN/OUT ARGS : None ***
+    *** RETURN : None ***
+    ********************************************************************/
     void postorderView() const;
 
     private:
